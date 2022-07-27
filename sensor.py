dbd68889b88ddc543cdb41b98f59245ac4ba90d3
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-07-27 14:25:31 -0700

    Update sensor.py
    updated code to match sensors used in Vancouver PI rather than SV PI

--- sensor.py
+++ sensor.py
@@ -1,27 +1,39 @@
-from w1thermsensor import W1ThermSensor
+import board
+import adafruit_dht
 import time
 import requests
-from requests.auth import HTTPBasicAuth
 
 '''
-Gets the current local time and the current temperature in celsius as measured by the temperature sensor.
+Gets the current local time and the current humidity and temperature in celsius as measured by the temperature sensor.
 Initiates a POST request to the web server, sends the time/temperature data as a json object.
 Requires basic authorization to connect to the server, and verifies the server's SSL certificate.
 Loops continuously and updates the time/temperature every 5 seconds.
 
 '''
 
+dhtDevice = adafruit_dht.DHT22(board.D4)
 
-def send_sensor_data():
-    sensor = W1ThermSensor()
-    while True:
+while True:
+    try:
         t = time.localtime()
-        current_time = time.strftime("%H:%M:%S", t)
-        temperature = sensor.get_temperature()
-        current_temp_celsius = str.format("%.2f" % temperature)
+        temp = dhtDevice.temperature
+        humidity = dhtDevice.humidity
 
-        data = {"time": current_time, "temperature": current_temp_celsius, "humidity": None}
+        current_time = time.strftime("%H:%M:%S", t)
+        temperature_c = str.format("%.2f" % temp)
+
+        data = {"time": current_time, "temperature": temperature_c, "humidity": humidity}
 
         url = "https://ec2-54-152-195-135.compute-1.amazonaws.com:8800/add_sensor_record"
         requests.post(url, json=data)
         time.sleep(5)
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+    
+
+
